c52856d03d3f1092003cfd8407cb66e63c7284e0
hatoriren <anastasianerestenko@gmail.com>
2022-12-10 22:39:45 +0100

    Updated JS

--- src/app.js
+++ src/app.js
@@ -11,8 +11,37 @@ function getTemperature(response) {
   let windSpeed = document.querySelector("#wind");
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
 }
-
+let city = "Paris";
 let apiKey = "277efc90731af2437305b7o4905bt1d3";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(getTemperature);
+
+let now = new Date();
+
+let dateElement = document.querySelector("#date");
+let timeElement = document.querySelector("#time");
+
+let date = now.getDate();
+
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let month = months[now.getMonth()];
+let days = ["Sun", "Mon", "Thue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+dateElement.innerHTML = `${date} ${month}, ${day}`;
+let hours = now.getHours();
+let minutes = now.getMinutes();
+timeElement.innerHTML = `${hours}:${minutes}`;
